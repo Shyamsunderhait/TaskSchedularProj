027f346eeafa0771b6900a4a421de7db459b2576
Shyamsunderhait <shyamsunderhait@gmail.com>
2024-05-07 03:35:06 +0530

    Project Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Appointment, AppService } from './app.service';
+import { DxSchedulerModule } from 'devextreme-angular/ui/scheduler';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DxSchedulerModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [AppService],
 })
 export class AppComponent {
   title = 'TaskSchedularProj';
+  appointments: Appointment[];
+  currentDate: Date = new Date(2024, 4, 7);
+
+  constructor(service: AppService) {
+    this.appointments = service.getAppointments();
+  }
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+
+export class Appointment {
+  title: any;
+  startDate: any;
+  endDate: any;
+  dayLong?: boolean;
+  recurrence?: string;
+}
+
+const appointments: Appointment[] = [
+  {
+    title: 'Battle of Panipat Strategy Meeting',
+    startDate: new Date('2024-05-07T08:45:00.000Z'),
+    endDate: new Date('2024-05-07T09:45:00.000Z'),
+  },
+  {
+    title: 'Gandhis satyagraha planning',
+    startDate: new Date('2024-05-08T08:00:00.000Z'),
+    endDate: new Date('2024-05-08T10:30:00.000Z'),
+  },
+  {
+    title: 'Indian Rebellion Testing',
+    startDate: new Date('2024-05-11T09:00:00.000Z'),
+    endDate: new Date('2024-05-11T10:00:00.000Z'),
+    recurrence: 'FREQ=WEEKLY;INTERVAL=1;COUNT=2',
+  },
+  {
+    title: 'Constitution Drafting Committee Meeting',
+    startDate: new Date('2024-05-22T10:00:00.000Z'),
+    endDate: new Date('2024-05-22T11:15:00.000Z'),
+    recurrence: 'FREQ=DAILY;BYDAY=WE;UNTIL=20211001',
+  },
+  {
+    title: 'Recruitment for Indian Armed Forces',
+    startDate: new Date('2024-05-23T08:00:00.000Z'),
+    endDate: new Date('2024-05-23T09:00:00.000Z'),
+    recurrence: 'FREQ=YEARLY',
+  },
+  {
+    title: 'Freedom Struggle Monthly Strategy',
+    startDate: new Date('2024-05-24T09:30:00.000Z'),
+    endDate: new Date('2024-05-24T10:45:00.000Z'),
+    recurrence: 'FREQ=MONTHLY;BYMONTHDAY=28;COUNT=1',
+  },
+  {
+    title: 'Republic Day Celebrations',
+    startDate: new Date('2024-05-09T09:30:00.000Z'),
+    endDate: new Date('2024-05-09T19:00:00.000Z'),
+  },
+];
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AppService {
+  getAppointments(): Appointment[] {
+    return appointments;
+  }
+}
